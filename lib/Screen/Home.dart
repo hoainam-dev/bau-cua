20c777d9df730814bf0de0775083c43a49638bc3
hoainam-dev <nam54529@donga.edu.vn>
2023-05-30 18:34:49 +0700

    [namhh][dev][bau-cua]_commit update code

--- lib/Screen/Home.dart
+++ lib/Screen/Home.dart
@@ -1,8 +1,19 @@
 import 'dart:math';
 
+import 'package:baucua/Screen/Auth/login_google.dart';
 import 'package:baucua/Screen/Profile/profile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+//map item {keys: name, value: coins}
+Map<String, double> mapItems = {
+  'nai': 0,
+  'bau': 0,
+  'ga': 0,
+  'ca': 0,
+  'cua': 0,
+  'tom': 0
+};
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,72 +25,86 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final User? user = FirebaseAuth.instance.currentUser;
 
-  //map item
-  Map<String, double> _mapItems = {
-    'nai': 0,
-    'bau': 0,
-    'ga': 0,
-    'ca': 0,
-    'cua': 0,
-    'tom': 0
-  };
-
-
-  List<String> _randomItems = [
-    'nai',
-    'nai',
-    'nai',
-  ];
+  late List<String> randomList = [];
 
   Map<String, double> _mapBet = {'': 0};
 
-  String _randomItem1 = 'nai';
-  String _randomItem2 = 'nai';
-  String _randomItem3 = 'nai';
+  final Random _random = Random();
 
-  TextEditingController _betController = new TextEditingController();
-  double _betAmounttotal = 0;
-  double _totalCoins = 100;
+  //declare randomIndex
+  late int _randomIndex1;
+  late int _randomIndex2;
+  late int _randomIndex3;
 
-  int _randomNum = 0;
+  //declare randomItem
+  late String _randomItem1;
+  late String _randomItem2;
+  late String _randomItem3;
 
-  Random _random = Random();
+  //Init
+  @override
+  void initState() {
+    //set state init index for dice 1,2,3
+    _randomIndex1 = _random.nextInt(6);
+    _randomIndex2 = _random.nextInt(6);
+    _randomIndex3 = _random.nextInt(6);
 
-  void _randomRecursion(int randomNum){
-    if (randomNum%6<6) // điều kiện dừng
+    //set state init item for dice 1,2,3
+    _randomItem1 = mapItems.keys.toList()[_randomIndex1];
+    _randomItem2 = mapItems.keys.toList()[_randomIndex2];
+    _randomItem3 = mapItems.keys.toList()[_randomIndex3];
+    // TODO: implement initState
+    super.initState();
+  }
+
+  TextEditingController _betController = new TextEditingController();//coins bet ipnut
+  double _betAmountTotal = 0;//coins bet total
+  double _totalCoins = 100;//coins total
+
+  //recursive function to random according to the rule
+  int _randomRecursion(int randomNum){
+    if (randomNum>5) //recursive exit condition
         {
-      _randomRecursion(randomNum);
+      randomNum = randomNum-6;
+      return _randomRecursion(randomNum);
     }
-    randomNum = randomNum%6;
+    return randomNum;
   }
 
   void _roll() {
     setState(() {
-      //random khong theo quy luat
-      _randomItem1 = _mapItems.keys.toList()[_random.nextInt(_mapItems.length)];
-      _randomItem2 = _mapItems.keys.toList()[_random.nextInt(_mapItems.length)];
+      //set index for dice3 by rule
+      _randomIndex3 = _randomIndex1+_randomIndex2+_randomIndex3+1;
+      _randomRecursion(_randomIndex3);
+      _randomItem3 = mapItems.keys.toList()[_randomRecursion(_randomIndex3)];
 
-      //random theo quy luat
+      //random index for dice1 and dice2
+      _randomIndex1 = _random.nextInt(mapItems.length);
+      _randomIndex2 = _random.nextInt(mapItems.length);
 
+      //random index for dice1 and dice2
+      _randomItem1 = mapItems.keys.toList()[_randomIndex1];
+      _randomItem2 = mapItems.keys.toList()[_randomIndex2];
 
-      _randomItem3 = _mapItems.keys.toList()[_random.nextInt(_mapItems.length)];
+      //clear old list and add new item into list before random
+      randomList.clear();
+      randomList.add(_randomItem1);
+      randomList.add(_randomItem2);
+      randomList.add(_randomItem3);
 
-      _randomItems.clear();
-      _randomItems.add(_randomItem1);
-      _randomItems.add(_randomItem2);
-      _randomItems.add(_randomItem3);
-
-      for (var randomItem in _randomItems) {
+      //check bet
+      for (var randomItem in randomList) {
         _mapBet.forEach((i, value) {
           if (randomItem == i) {
             _totalCoins += value * 2;
           }
         });
       }
+
       //set map to default
       _mapBet.clear();
-      _betAmounttotal = 0;
-      _mapItems = {
+      _betAmountTotal = 0;
+      mapItems = {
         'nai': 0,
         'bau': 0,
         'ga': 0,
@@ -90,6 +115,7 @@ class _HomeState extends State<Home> {
     });
   }
 
+  //Function show widget to input bet
   bet(String item) async {
     await showModalBottomSheet(
         isScrollControlled: true,
@@ -115,23 +141,23 @@ class _HomeState extends State<Home> {
                   onPressed: () async {
                     final String _bet = _betController.text;
                     if (_bet != null && double.parse(_bet) <= _totalCoins) {
-                      _mapBet[item] = double.parse(_bet);
+                      double coinsBet = 0;
+                      if(_mapBet[item]!=null){
+                        coinsBet = _mapBet[item]!;
+                      }
+                      _mapBet[item] = double.parse(_bet) + coinsBet;
                       setState(() {
-                        _mapItems[item] = double.parse(_bet);
+                        mapItems[item] = double.parse(_bet) + coinsBet;
                       });
                       _totalCoins -= double.parse(_bet);
-                      _betAmounttotal += double.parse(_bet);
+                      _betAmountTotal += double.parse(_bet);
                       Navigator.of(context).pop();
                     } else {
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                           content: Text(
                               'Số dư tài khoản không đủ. Vui lòng nạp thêm tiền.',
                               style: TextStyle(color: Colors.red))));
-                      // Navigator.of(context).pop();
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) => super.widget));
+                      Navigator.of(context).pop();
                     }
                   },
                 )
@@ -164,68 +190,105 @@ class _HomeState extends State<Home> {
         ),
         body: SingleChildScrollView(
             child: Column(
-          children: <Widget>[
-            const SizedBox(height: 10),
-            StreamBuilder<User?>(
-                stream: FirebaseAuth.instance.authStateChanges(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Text(" Wellcome ${user!.email}", style: const TextStyle(fontSize: 16));
-                  } else
-                    return Text("");
-                }),
-            const SizedBox(height: 20),
-            Container(
-                height: 300,
-                width: MediaQuery.of(context).size.width,
-                child: GridView.count(
-                    crossAxisCount: 3,
-                    children: List.generate(_mapItems.length, (index) {
-                      return Center(
-                          child: Container(
-                              child: ConstrainedBox(
-                                  constraints: const BoxConstraints.expand(),
-                                  child: FlatButton(
-                                    onPressed: () {
-                                      bet(_mapItems.keys.toList()[index]);
-                                    },
-                                    child: Column(
-                                      children: [
-                                        Image.asset(
-                                            'assets/img/${_mapItems.keys.toList()[index]}.png',
-                                            height: 110,
-                                            width: 110),
-                                        Text(
-                                            "${_mapItems.values.toList()[index]}")
-                                      ],
-                                    ),
-                                  ))));
-                    }))),
-            Text("Tổng tiền cược: ${_betAmounttotal}", style: TextStyle(fontSize: 16)),
-            SizedBox(height: 10),
-            const Text("Kết quả lắc", style: TextStyle(fontSize: 16)),
-            SizedBox(height: 10),
-            Container(
-                height: 150,
-                width: MediaQuery.of(context).size.width,
-                child: GridView.count(
-                    crossAxisCount: 3,
-                    children: List.generate(_randomItems.length, (index) {
-                      return Image.asset('assets/img/${_randomItems[index]}.png',
-                          height: 90, width: 90);
-                    }))),
-            Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: _roll,
-                      child: const Text('Lắc'),
-                    ),
-                  ],
-                )),
-          ],
-        )));
+              children: <Widget>[
+                const SizedBox(height: 10),
+                StreamBuilder<User?>( //check haven been login
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Text(" Wellcome ${user!.email}", style: const TextStyle(fontSize: 16));
+                      } else
+                        return Text("");
+                    }),
+                const SizedBox(height: 10),
+                Text("Số dư: ${_totalCoins}"),
+                const SizedBox(height: 20),
+                StreamBuilder<User?>( //check haven been login
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Container(
+                            height: 300,
+                            width: MediaQuery.of(context).size.width,
+                            child: GridView.count(
+                                crossAxisCount: 3,
+                                children: List.generate(mapItems.length, (index) {
+                                  return Center(
+                                      child: Container(
+                                          child: ConstrainedBox(
+                                              constraints: const BoxConstraints.expand(),
+                                              child: FlatButton(
+                                                onPressed: () {
+                                                  bet(mapItems.keys.toList()[index]);
+                                                },
+                                                child: Column(
+                                                  children: [
+                                                    Image.asset(
+                                                        'assets/img/${mapItems.keys.toList()[index]}.png',
+                                                        height: 110,
+                                                        width: 110),
+                                                    Text(
+                                                        "${mapItems.values.toList()[index]}")
+                                                  ],
+                                                ),
+                                              ))));
+                                })));
+                      } else
+                        return Container(
+                            height: 300,
+                            width: MediaQuery.of(context).size.width,
+                            child: GridView.count(
+                                crossAxisCount: 3,
+                                children: List.generate(mapItems.length, (index) {
+                                  return Center(
+                                      child: Container(
+                                          child: ConstrainedBox(
+                                              constraints: const BoxConstraints.expand(),
+                                              child: FlatButton(
+                                                onPressed: () {
+                                                  Navigator.push(context,
+                                                      MaterialPageRoute(builder: (context) =>
+                                                          LoginWithGoogle()));
+                                                },
+                                                child: Column(
+                                                  children: [
+                                                    Image.asset(
+                                                        'assets/img/${mapItems.keys.toList()[index]}.png',
+                                                        height: 110,
+                                                        width: 110),
+                                                    Text(
+                                                        "${mapItems.values.toList()[index]}")
+                                                  ],
+                                                ),
+                                              ))));
+                                })));
+                    }),
+                Text("Tổng tiền cược: ${_betAmountTotal}", style: TextStyle(fontSize: 16)),
+                SizedBox(height: 10),
+                const Text("Kết quả lắc", style: TextStyle(fontSize: 16)),
+                SizedBox(height: 10),
+                randomList.length>0?
+                Container(
+                    height: 150,
+                    width: MediaQuery.of(context).size.width,
+                    child: GridView.count(
+                        crossAxisCount: 3,
+                        children: List.generate(randomList.length, (index) {
+                          return Image.asset('assets/img/${randomList[index]}.png',
+                              height: 90, width: 90);
+                        }))):Container(),
+                Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: _roll,
+                          child: const Text('Lắc'),
+                        ),
+                      ],
+                    )),
+              ],
+            )));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:baucua/Screen/BauCuaScreen.dart';
 import 'package:baucua/Screen/Home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
